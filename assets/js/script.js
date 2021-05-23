690528800aa51d9c4f24294c23f540b7e85a0fe4
Jsieler <joshuasieler@Joshs-Air.home>
2021-05-23 11:42:12 -0600

    setting up functions ill need to add

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,28 @@ var submitButtonEl = document.querySelector("#submit");
 var showHighScoresEl = document.querySelector("#showHighScores");
 var answerCorrectEl = document.querySelector("#answer-correct");
 var answerIncorrectEl = document.querySelector("#answer-incorrect");
+
+// Questions 
+const questions = []
+
+// Start function on click
+var startQuiz = function();
+
+// timer
+var timeLeft = 75;
+var score = 0;
+var questionIndex = 0;
+highScores[];
+
+// timer function
+var timer = function();
+
+// function to clear and populate next question and answers to lift 
+var nextQuestion = function();
+
+// function to check answers
+var checkAnswer = function();
+
+// function to end quiz if timer is 0
+var endQuiz = function();
+
